3710b8646c7588db785dbda8728969ee4a265fb9
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-09-26 19:32:49 +0200

    editor fix

--- bin/local.sh
+++ bin/local.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-rm ~/kiwilan-vite-plugin-laravel-steward-0.0.135.tgz ; pnpm build ; npm pack --pack-destination ~
+rm ~/kiwilan-vite-plugin-laravel-steward-0.0.135.tgz
+pnpm build
+npm pack --pack-destination ~
+
+# In project, add
+# `"@kiwilan/vite-plugin-laravel-steward": "file:~/kiwilan-vite-plugin-laravel-steward-0.0.135.tgz",`
